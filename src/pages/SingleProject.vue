1d0c10aa63bd873ab750fd78bc214ed599bed681
Alessandro Gattuso <alessandrogattuso@yahoo.it>
2023-03-10 17:39:30 +0100

    added some styling to singleproject page

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -23,14 +23,31 @@ export default {
 <template lang="">
   <div class="container">
     <div v-if="loading"></div>
-    <div v-else>
-      <div class="row">
-        <div class="col-12">
-          <h4>{{ project.title }}</h4>
-        </div>
-        <div class="cover-img">
-          <img :src="project.cover_image ? `${this.store.baseUrl}/storage/${project.cover_image}` : 'https://picsum.photos/300/200'" class="img-fluid" alt="">
-        </div>
+    <div v-else class="d-flex">
+        <div class="row g-0 mt-4 d-flex">
+          <div class="col-4">
+            <img :src="project.cover_image ? `${this.store.baseUrl}/storage/${project.cover_image}` : 'https://picsum.photos/300/200'" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-5">
+            <div class="card-body">
+              <h5 class="card-title">{{ project.title }}</h5>
+              <p class="card-text">{{ project.description }}</p>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item p-0" v-if="project.type">Tipo: {{ project.type.name }}</li>
+                <li class="list-group-item p-0" v-if="project.technologies.length">
+                    Tecnologie: 
+                    <div class="d-flex flex-wrap gap-4">
+                     <span class="badge bg-primary" v-for="(tech, index) in project.technologies" :key="index">{{ tech.name }}</span>
+                    </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col d-flex justify-content-end">
+            <div>
+              <router-link :to="{ name: 'homepage' }" class="btn btn-primary">Go back</router-link>
+            </div>
+          </div>
       </div>
     </div>
   </div>
